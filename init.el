aacd4fa1ab40010957c72b984564af0844303135
Myoungdo Park <cuspymd@gmail.com>
2018-02-12 16:48:55 +0000

    Disable visible-bell

--- init.el
+++ init.el
@@ -51,7 +51,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(flycheck-idle-change-delay 1.0)
- '(package-selected-packages (quote (company tide material-theme better-defaults))))
+ '(package-selected-packages (quote (company tide material-theme better-defaults)))
+ '(visible-bell nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
