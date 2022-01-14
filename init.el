ffcf709294e83ee9690d76a03a8e82e1c5cae156
Oracle Public Cloud User <opc@instance-cuspymd.subnet01070036.vcn01070036.oraclevcn.com>
2022-01-14 18:03:22 +0000

    Sync latest

--- init.el
+++ init.el
@@ -1,8 +1,3 @@
-;; init.el --- Emacs configuration
-
-;; INSTALL PACKAGES
-;; --------------------------------------
-
 (require 'package)
 
 (add-to-list 'package-archives
@@ -12,22 +7,11 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(defvar myPackages
-  '(better-defaults
-    elpy
-    material-theme))
-
-(mapc #'(lambda (package)
-    (unless (package-installed-p package)
-      (package-install package)))
-      myPackages)
-
-;; BASIC CUSTOMIZATION
-;; --------------------------------------
-
 (setq inhibit-startup-message t) ;; hide the startup message
 (load-theme 'material t) ;; load material theme
 (require 'better-defaults)
+(load custom-file)
+(setq ido-everywhere t)
 
 (defun setup-tide-mode ()
   (interactive)
@@ -45,61 +29,9 @@
 (setq company-tooltip-align-annotations t)
 
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
-;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector
-   (vector "#212121" "#B71C1C" "#558b2f" "#FFA000" "#2196f3" "#4527A0" "#00796b" "#FAFAFA"))
- '(custom-enabled-themes (quote (material)))
- '(custom-safe-themes
-   (quote
-    ("a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" default)))
- '(default-input-method "korean-hangul")
- '(elm-indent-offset 2)
- '(fci-rule-color "#ECEFF1")
- '(flycheck-idle-change-delay 1.0)
- '(hl-sexp-background-color "#efebe9")
- '(package-selected-packages
-   (quote
-    (rjsx-mode multi-term yaml-mode smartparens rainbow-delimiters paredit cider clojure-mode elm-mode web-mode company tide material-theme better-defaults)))
- '(show-paren-mode t)
- '(tool-bar-mode nil)
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#B71C1C")
-     (40 . "#FF5722")
-     (60 . "#FFA000")
-     (80 . "#558b2f")
-     (100 . "#00796b")
-     (120 . "#2196f3")
-     (140 . "#4527A0")
-     (160 . "#B71C1C")
-     (180 . "#FF5722")
-     (200 . "#FFA000")
-     (220 . "#558b2f")
-     (240 . "#00796b")
-     (260 . "#2196f3")
-     (280 . "#4527A0")
-     (300 . "#B71C1C")
-     (320 . "#FF5722")
-     (340 . "#FFA000")
-     (360 . "#558b2f"))))
- '(vc-annotate-very-old-color nil)
- '(visible-bell nil))
 
 (elpy-enable)
 (add-hook 'clojure-mode-hook #'paredit-mode)
 (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
 (set-fontset-font t 'hangul (font-spec :name "NanumGothicCoding"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Noto Mono" :foundry "GOOG" :slant normal :weight normal :height 108 :width normal)))))
+
