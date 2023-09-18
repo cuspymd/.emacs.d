c241f16637883ae1bf51cd1b41512ef6937768ae
Myoungdo Park <cuspymd@gmail.com>
2023-09-18 23:57:31 +0900

    Fix key error on consult

--- custom.el
+++ custom.el
@@ -32,4 +32,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 113 :width normal)))))
+ '(default ((t (:family "FiraCode Nerd Font Mono" :foundry "CTDB" :slant normal :weight regular :height 113 :width normal)))))
